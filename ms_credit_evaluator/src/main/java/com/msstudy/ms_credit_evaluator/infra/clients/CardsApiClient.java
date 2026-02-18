47d724a39bc95290db94b07da7b62cbe5f1a2f8b
Kayne <kayne.ferreira@outlook.com>
2026-02-18 15:02:25 -0300

    Project updated: - Implementado funcionalidade de vinculo para lista de cartões de   clientes;

---
+++ ms_credit_evaluator/src/main/java/com/msstudy/ms_credit_evaluator/infra/clients/CardsApiClient.java
@@ -0,0 +1,18 @@
+package com.msstudy.ms_credit_evaluator.infra.clients;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.msstudy.ms_credit_evaluator.domain.model.CustomerCard;
+
+@FeignClient(value = "ms-cards", path = "/cards")
+public interface CardsApiClient {
+	
+	@GetMapping(params = "cpf")
+    ResponseEntity<List<CustomerCard>> customerCardData(@RequestParam String cpf);
+
+}
--- ms_credit_evaluator/src/main/java/com/msstudy/ms_credit_evaluator/service/CreditEvaluatorService.java
+++ ms_credit_evaluator/src/main/java/com/msstudy/ms_credit_evaluator/service/CreditEvaluatorService.java
@@ -1,10 +1,14 @@
 package com.msstudy.ms_credit_evaluator.service;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.msstudy.ms_credit_evaluator.domain.model.CustomerCard;
 import com.msstudy.ms_credit_evaluator.domain.model.CustomerData;
 import com.msstudy.ms_credit_evaluator.domain.model.CustomerStatus;
+import com.msstudy.ms_credit_evaluator.infra.clients.CardsApiClient;
 import com.msstudy.ms_credit_evaluator.infra.clients.CustomerApiClient;
 
 import lombok.RequiredArgsConstructor;
@@ -14,12 +18,17 @@ import lombok.RequiredArgsConstructor;
 public class CreditEvaluatorService {
 	
 	private final CustomerApiClient customerClient;
+	private final CardsApiClient cardClient;
 
     public CustomerStatus getCustomerStatus(String cpf) {
+    	
     	ResponseEntity<CustomerData> customerDataResponse = customerClient.customerData(cpf);
+    	ResponseEntity<List<CustomerCard>> cardDataResponse = cardClient.customerCardData(cpf);
+    	
     	return CustomerStatus
     			.builder()
     			.customerData(customerDataResponse.getBody())
+    			.customerCard(cardDataResponse.getBody())
     			.build();
     }
     
